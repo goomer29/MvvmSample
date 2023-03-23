e9c58d01a1ddd421e4ce660a8752537e4cc3c93b
User <User@TEACHER3>
2023-03-23 15:23:37 +0200

    Full Solution

--- MvvmSample/ViewModels/MainPageViewModel.cs
+++ MvvmSample/ViewModels/MainPageViewModel.cs
@@ -12,35 +12,43 @@ using System.Windows.Input;
 
 namespace MvvmSample.ViewModels
 {
-    public class MainPageViewModel : INotifyPropertyChanged
+    public class MainPageViewModel : ViewModel
     {
-        #region מימוש ממשק עדכון שינויים
-        public event PropertyChangedEventHandler PropertyChanged;                        
-        
-        private  void OnPropertyChanged([CallerMemberName] string propertyName="")
-        {
-             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));         
-        }
-        #endregion
-
-
         public ObservableCollection<Student> Students { get; set; }
 
         public ICommand LoadDataCommand { get; private set; } 
 
         public ICommand ClearDataCommand { get; private set; }
 
+        public ICommand RefreshCommand {  get; private set; }
+        public ICommand AddStudentCommand { get; private set; }
+
+
+
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+        private bool isRefreshing;
+
+        public bool IsRefreshing { get { return isRefreshing; } set { if (isRefreshing != value) { isRefreshing = value; OnPropertyChanged(); }; } }
+
         public MainPageViewModel()
         {
             Students = new ObservableCollection<Student>(); 
             LoadDataCommand=new Command(LoadData);
             ClearDataCommand = new Command(() => Students.Clear());
+            RefreshCommand = new Command(LoadData);
+            AddStudentCommand = new Command(() => Students.Add(new Student() { BirthDate = BirthDate, Name = Name, Image = $"{Name}.jpg" }));
+           
         }
 
         public void LoadData()
         {
             Students.Clear();
-           Students.Add(new Student() { BirthDate = DateTime.Now, Name = "ג'ופיר", Image = "jofir.jpg" });
+           Students.Add(new Student() { BirthDate = new DateTime(2006,5,28), Name = "ג'ופיר", Image = "jofir.jpg" });
+            IsRefreshing = false;
+            
             
         }
     }
---
+++ MvvmSample/ViewModels/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmSample.ViewModels
+{
+    public class ViewModel:INotifyPropertyChanged
+    {
+        #region מימוש ממשק עדכון שינויים
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
